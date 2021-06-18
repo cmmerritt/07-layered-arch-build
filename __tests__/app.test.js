f84f1c6b7ec6cf25dd1313a297eb3180d275fb32
cmmerritt <christiane.merritt@gmail.com>
2021-06-18 14:59:15 -0700

    adjust quote function to return author as well

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,9 +10,11 @@ dotenv.config();
 
 const testQuote = 'A theatre is the most important sort of house in the world, because that\'s where people are shown what they could be if they wanted, and what they\'d like to be if they dared to and what they really are';
 
+const testQuoteAndAuthor = 'A theatre is the most important sort of house in the world, because that\'s where people are shown what they could be if they wanted, and what they\'d like to be if they dared to and what they really are - Tove Jansson';
+
 describe('quote API munging', () => {
   it('munges quote', async () => {
-    const expected = testQuote;
+    const expected = testQuoteAndAuthor;
     const actual = formatQuote(quoteData);
     expect(actual).toEqual(expected);
   });
