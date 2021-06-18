830d442492cb92b9c9063ad152b7028b02038f4f
cmmerritt <christiane.merritt@gmail.com>
2021-06-18 10:14:43 -0700

    add munge API data function

---
+++ data/quoteData.js
@@ -0,0 +1,11 @@
+export default {
+  'id':22008,
+  'language_code':'en',
+  'content':'People really need help but may attack you if you help them. Help people anyway!',
+  'url':'https://quotepark.com/quotes/22008-matka-tereza-people-really-need-help-but-may-attack-you-if-you/',
+  'originator':{
+    'id':164750,
+    'name':'Matka Tereza',
+    'url':'https://quotepark.com/authors/matka-tereza/'
+  },
+};
